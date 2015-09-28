9fb634634cd416e98abe4f21a595f0723037e805
Jun Jen Chan <junjenchan@gmail.com>
2015-09-29 01:47:46 +1000

    add image icon files in repo and remove unused code files

---
+++ app/src/main/java/co/example/junjen/mobileinstagram/NavigationBar.java
@@ -0,0 +1,83 @@
+package co.example.junjen.mobileinstagram;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+public class NavigationBar extends AppCompatActivity {
+
+    FragmentTransaction ft;
+    UserFeedFragment userFeedFragment;
+    DiscoverFragment discoverFragment;
+    CameraFragment cameraFragment;
+    ActivityFeedFragment activityFeedFragment;
+    ProfileFragment profileFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_bar);
+
+        userFeedFragment = new UserFeedFragment();
+        discoverFragment = new DiscoverFragment();
+        cameraFragment = new CameraFragment();
+        activityFeedFragment = new ActivityFeedFragment();
+        profileFragment = new ProfileFragment();
+
+        RadioButton userFeedButton = (RadioButton) findViewById(R.id.userFeed_rbutton);
+        userFeedButton.setChecked(true);
+        getSupportFragmentManager().beginTransaction().add(R.id.view1, userFeedFragment).commit();
+
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.nav_bar);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                ft = getSupportFragmentManager().beginTransaction();
+                switch (checkedId) {
+                    case R.id.userFeed_rbutton:
+                        ft.replace(R.id.view1, userFeedFragment);
+                        break;
+                    case R.id.discover_rbutton:
+                        ft.replace(R.id.view1, discoverFragment);
+                        break;
+                    case R.id.camera_rbutton:
+                        ft.replace(R.id.view1, cameraFragment);
+                        break;
+                    case R.id.activityFeed_rbutton:
+                        ft.replace(R.id.view1, activityFeedFragment);
+                        break;
+                    case R.id.profile_rbutton:
+                        ft.replace(R.id.view1, profileFragment);
+                        break;
+                }
+                ft.commit();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_navigation_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
